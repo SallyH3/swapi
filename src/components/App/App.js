18d15fd4555536740641426458bcf36b2023fb28
Sally Haefling <sallyhaefling@gmail.com>
2019-05-01 20:01:56 -0600

    Start work on people fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,7 @@ import Favorites from '../Favorites/Favorites';
 import Header from '../Header/Header';
 import Controls from '../Controls/Controls';
 import Scroll from '../Scroll/Scroll';
-
+import Card from '../Card/Card';
 class App extends Component {
   constructor() {
     super();
@@ -14,15 +14,16 @@ class App extends Component {
         year: '',
         crawl: ''
       },
+      people: [],
       isLoading: false
     }
   }
-
+  
   getRandomFilm = () => {
     return Math.floor((Math.random() * 7) + 1);
   }
-
-  getFilmInfo = (film) => {
+  
+  getCrawlFilmInfo = (film) => {
     return {
       title: film.title,
       year: film.release_date,
@@ -35,23 +36,48 @@ class App extends Component {
     const url = `https://swapi.co/api/films/${randomFilm}`;
     fetch(url)
       .then(response => response.json())
-      .then(film => this.getFilmInfo(film))
+      .then(film => this.getCrawlFilmInfo(film))
       .then(currentFilm => this.setState({ currentFilm }))
       .catch(error => console.log(error))
   }
 
+  getHomeworld = (people) => {
+    //when console.logging, it doesnt know what people is...says cannot read results of undefined, need to fix
+    const getHomeworld = people.results.map(person => {
+      return fetch(person.homeworld)
+      .then(result => ({...person, homeworld: result.name, population: result.population}))
+    })
+    return Promise.all(getHomeworld)
+  }
+
+  //need to figure out how to get people correctly, this isnt working below...
+
+  displayPeople = () => {
+    const url = `https://swapi.co/api/people/`;
+    fetch(url)
+      .then(response => response.json())
+      .then(people => this.getHomeworld(people))
+      // .then(people => this.getSpecies)
+      // .then(people => this.getPopulation)
+      .then(people => this.setState({people: people}))
+      .catch(error => console.log(error))
+  }
+
   render() {
+    console.log(this.displayPeople())
     const {title, year, crawl} = this.state.currentFilm;
     return (
       <div className="App">
         <Favorites />
         <Header />
-        <Controls />
+        {/* adding displayPeople to controls below...fn still needs to be fixed */}
+        <Controls displayPeople = {this.displayPeople}/>
         <Scroll
           title={title}
           year={year} 
           crawl={crawl} 
         />
+        <Card />
       </div>
     );
   }
